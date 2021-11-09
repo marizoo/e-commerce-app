f6c2ef02796ef2803f29b346cf624e1f1fe369ca
marizoo <marizoo@meow.woof>
2021-11-09 23:25:48 +0700

    finished login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
 
-import ProductPg from "./pages/ProductPg";
+import Login from "./pages/Login";
+
+// import Register from "./pages/Register";
+
+// import ProductPg from "./pages/ProductPg";
 
 const App = () => {
   return (
   <>
-    <ProductPg />
+    <Login />
   </>
   );
 };
