a3c0f9c06d9c800564249705cc162e66a89c31d7
marizoo <marizoo@meow.woof>
2021-11-09 12:39:15 +0700

    added product components

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,6 +2,23 @@ import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@m
 import React from 'react'
 import styled from 'styled-components'
 
+
+const Info = styled.div`
+opacity: 0;
+width: 100%;
+height: 100%;
+position: absolute;
+top: 0;
+left: 0;
+background-color: rgba(0, 0, 0, 0.2);
+z-index: 3;
+display: flex;
+align-items: center;
+justify-content: center;
+transition: all 0.5s ease;
+cursor: pointer;
+`
+
 const Cont = styled.div`
 flex: 1;
 margin: 5px;
@@ -12,6 +29,10 @@ align-items: center;
 justify-content: center;
 background-color: #f5fbfd;
 position: relative;
+
+&:hover ${Info}{
+    opacity: 1;
+}
 `
 
 const Circle = styled.div`
@@ -26,19 +47,6 @@ height: 75%;
 z-index: 2;
 `
 
-const Info = styled.div`
-width: 100%;
-height: 100%;
-position: absolute;
-top: 0;
-left: 0;
-background-color: gray;
-z-index: 3;
-display: flex;
-align-items: center;
-justify-content: center;
-`
-
 const Icon = styled.div`
 width: 40px;
 height: 40px;
@@ -48,6 +56,12 @@ display: flex;
 align-items: center;
 justify-content: center;
 margin: 10px;
+transition: all 0.5s ease;
+
+&:hover {
+    background-color: #e9f5f5;
+    transform:scale(1.1);
+}
 `
 // https://www.youtube.com/watch?v=c1xTDSIXit8&t=77s
 // min 1:10:45
